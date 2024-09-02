2cd7b8ba628fbc5de98b169bc3a9fb3b824bbdee
ANURAG <patilanurag13@gmail.com>
2024-09-02 11:08:16 +0530

    added on sep2

--- Website/urls.py
+++ Website/urls.py
@@ -6,5 +6,6 @@ from Website import views
 urlpatterns = [
         path('',views.index,name='index'),
         path('login',views.login,name='login'),
-        path('register',views.register,name='register')
+        path('register',views.register,name='register'),
+        path("login_check",views.login_check.as_view(),name='login_check'),
     ]
--- Website/views.py
+++ Website/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from django.http import JsonResponse
 
 def index(request):
     return render(request,"TEST_HOME.html",context=None)
@@ -8,3 +10,12 @@ def login(request):
 
 def register(request):
     return render(request,"register.html",context=None)
+
+class login_check(APIView):  
+     def post(self, request):
+         username = request.POST['username']
+         password = request.POST['pass']
+         if username == 'test':
+            return JsonResponse({"status":"pass"})
+         else:
+             return JsonResponse({"status":"fail"})
